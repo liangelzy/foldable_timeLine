bee611ec1e100e6dfe5404c315d1122a36d77ea2
liuzhiyun <liuzhiyun@vergood.cn>
2023-06-25 17:12:56 +0800

    结构调整

---
+++ js/time_line.js
@@ -0,0 +1,177 @@
+var root_vm = new Vue({
+    el: '#content_search',
+    data: function () {
+        return {
+
+            tableData:[
+                {
+                    "year": "2013",
+                    "child": [
+                        {
+                            "contentCreateTime": "2022-08-17 16:22:52",
+                            "contentId": "earth_chinese-manned-shenzhou-10-spacecraft-lands-safely-in-inner-mongolia",
+                            "contentSource": "SPACE_REF",
+                            "url": "https://spaceref.com/earth/chinese-manned-shenzhou-10-spacecraft-lands-safely-in-inner-mongolia/",
+                            "content": "中国载人<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船安全着陆<span class=\"search_hit\">内蒙古</span>,CCTV&&China&&Shenzhou-10,中国<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>于美国东部时间晚上8：08（北京时间上午8：08）安全降落在<span class=\"search_hit\">内蒙古</span>，在经过15天的任务后将三名宇航员送回地球",
+                            "contentAuthor": "Marc Boucher",
+                            "docId": "earth_chinese-manned-shenzhou-10-spacecraft-lands-safely-in-inner-mongolia",
+                            "contentDateTime": "2013-06-25 00:00:00",
+                            "title": "中国载人神舟十号飞船安全着陆内蒙古",
+                            "contentTitleEn": "Chinese Manned Shenzhou-10 Spacecraft Lands Safely in Inner Mongolia"
+                        }
+                    ]
+                },
+                {
+                    "year": "2016",
+                    "child": [
+                        {
+                            "contentCreateTime": "2023-06-15 14:52:40",
+                            "contentId": "2650503963_3",
+                            "contentSource": "公众号",
+                            "url": "http://mp.weixin.qq.com/s?__biz=MzA3ODMxNTIxMA==&mid=2650503963&idx=3&sn=269d07fe6f75bb2e1297240568778ab9&chksm=874b70beb03cf9a89a7bd1e889438ae7e10e193758f4345c21b8f84a6ddd3c7e4104ee1d74b8#rd",
+                            "content": "<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>\n 中国首次太空授课活动举行\n <span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船成功发射\n <span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船及科研人员\n 航天员王亚平在进行太空授课\n 2013年6月11日17点38分，<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船搭载聂海胜、张晓光、王亚平三名航天员发射升空",
+                            "contentAuthor": "太空与网络",
+                            "docId": "2650503963_3",
+                            "contentDateTime": "2016-10-18 17:16:00",
+                            "title": "大盘点｜你看过几艘神舟飞船发射？"
+                        },
+                        {
+                            "contentCreateTime": "2023-06-15 14:52:40",
+                            "contentId": "2650502242_1",
+                            "contentSource": "公众号",
+                            "url": "http://mp.weixin.qq.com/s?__biz=MzA3ODMxNTIxMA==&mid=2650502242&idx=1&sn=97641924adda2575023d45e558ef6e16&chksm=874b7fc7b03cf6d15a1f99a0f9f07c6ac8f0e1b80fd2b25abb9a367fe6d7bc2cf6d7317dcdab#rd",
+                            "content": "航天员聂海胜、张晓光和王亚平将搭乘<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>载人飞船，执行第五次载人航天飞行任务。此前，已有8名航天员完成载人航天飞行任务。算上<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞天，我国共有10名上天航天员，其中3人已晋升为少将军衔。",
+                            "contentAuthor": "太空与网络",
+                            "docId": "2650502242_1",
+                            "contentDateTime": "2016-04-28 18:42:00",
+                            "title": "杨利伟曝出舱嘴角渗血细节 快顶不住了 有死亡感"
+                        }
+                    ]
+                },
+                {
+                    "year": "2018",
+                    "child": [
+                        {
+                            "contentCreateTime": "2023-06-15 14:52:29",
+                            "contentId": "2650515383_1",
+                            "contentSource": "公众号",
+                            "url": "http://mp.weixin.qq.com/s?__biz=MzA3ODMxNTIxMA==&mid=2650515383&idx=1&sn=384033fee83d3e1570283d798eab92fb&chksm=874bac12b03c25047dbf19ca7ab0f962b583f60c9d30213b0bf57ed32724fd451854ab407626#rd",
+                            "content": "新华社记者 李鑫 摄\n 2013年6月11日17时38分，<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船搭载聂海胜、张晓光、王亚平3名航天员发射升空。",
+                            "contentAuthor": "太空与网络",
+                            "docId": "2650515383_1",
+                            "contentDateTime": "2018-11-14 21:03:00",
+                            "title": "改革开放40年 | 回望40年中国航天的40件大事"
+                        },
+                        {
+                            "contentCreateTime": "2023-06-15 14:53:26",
+                            "contentId": "2650512335_2",
+                            "contentSource": "公众号",
+                            "url": "http://mp.weixin.qq.com/s?__biz=MzA3ODMxNTIxMA==&mid=2650512335&idx=2&sn=f76fd91490e140b6f1e7c308bc166e80&chksm=874b906ab03c197cc8b2ba8c91c0518b2fa97b406c05b4376e45f74ae942689842908e9e1681#rd",
+                            "content": "九、2013年6月，中国首次举行太空授课\n <span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>航天员进行太空授课\n 2013年6月11日，聂海胜、张晓光、王亚平3名航天员搭乘<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船由长征二号F运载火箭发射入轨；6月13日，<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船与天宫一号目标飞行器成功实现自动交会对接",
+                            "contentAuthor": "太空与网络",
+                            "docId": "2650512335_2",
+                            "contentDateTime": "2018-04-12 20:40:00",
+                            "title": "载人空间飞行国际日，这13件中国载人航天大事值得收藏！"
+                        }
+                    ]
+                },
+                {
+                    "year": "2021",
+                    "child": [
+                        {
+                            "contentCreateTime": "2023-04-17 15:24:47",
+                            "contentId": "tBFs9XR0",
+                            "contentSource": "SPACE_COM_content",
+                            "url": "https://www.space.com/shenzhou-12-astronaut-mission-lands-safely",
+                            "content": "天宫一号于2011年9月进入地球轨道，并主持了两次载人飞行任务，即2012年6月的神舟9号和2013年6月的<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>。天宫二号于2016年9月升空，并在下个月迎来神舟十一号宇航员进行为期30天的任务。",
+                            "contentAuthor": "Mike Wall",
+                            "docId": "tBFs9XR0",
+                            "contentDateTime": "2021-09-17 00:00:00",
+                            "title": "中国宇航员在新空间站执行为期3个月的历史性任务后着陆",
+                            "contentTitleEn": "Chinese astronauts land after historic 3-month mission to new space station"
+                        }
+                    ]
+                },
+                {
+                    "year": "2022",
+                    "child": [
+                        {
+                            "contentCreateTime": "2023-06-15 14:53:01",
+                            "contentId": "2248064592_2",
+                            "contentSource": "公众号",
+                            "url": "http://mp.weixin.qq.com/s?__biz=MzI2NzQ0OTgxMA==&mid=2248064592&idx=2&sn=5f247f301feedb4704504aeef58ac946&chksm=eaf4baeddd8333fbf48ce5d43d8c413698e020ce2e61246b39c9ba560eb019315bd678ffd156#rd",
+                            "content": "图为从空中俯视<span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船返回降落现场。\n 该书专业科普并茂，不仅描述了载人航天技术细节，也科普解释了技术内涵，浅显易懂，生动有趣，是一本不错的科普书。",
+                            "contentAuthor": "我们的太空",
+                            "docId": "2248064592_2",
+                            "contentDateTime": "2022-12-05 11:26:00",
+                            "title": "这两本书，讲述了“返回”的故事"
+                        }
+                    ]
+                },
+                {
+                    "year": "2023",
+                    "child": [
+                        {
+                            "contentCreateTime": "2023-06-16 03:00:14",
+                            "contentId": "2248121912_1",
+                            "contentSource": "公众号",
+                            "url": "http://mp.weixin.qq.com/s?__biz=MzI2NzQ0OTgxMA==&mid=2248121912&idx=1&sn=49dcba2286559776388faefa6f49e8ee&chksm=eaf59a85dd821393531eb51e0e9e0a8eae1c6365d65e4f807edb6aa40f010f5ddf2ec1666959#rd",
+                            "content": "手控交会对接和首次绕飞交会试验\n 6月26日\n <span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>飞船返回舱\n 顺利降落在<span class=\"search_hit\">内蒙古</span>中部主着陆场预定区域\n 3名航天员平安健康出舱\n <span class=\"search_hit\">神舟</span><span class=\"search_hit\">十号</span>成功返回\n 通过15天的在轨飞行\n 进一步考核和巩固了交会对接技术",
+                            "contentAuthor": "我们的太空",
+                            "docId": "2248121912_1",
+                            "contentDateTime": "2023-06-10 21:57:00",
+                            "title": "神舟十号，问天十年！"
+                        }
+                    ]
+                }
+            ],//列表数据
+            selectLoading: false,
+            pageNo: 1,
+            size: 500,
+            total: 0,
+            rules: {},
+            reverse: false,
+            activeName: 'first'
+        };
+    },
+    watch: {
+        reverse: function (newVal, oldVal) {
+            this.tableData = this.tableData.reverse();
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+        loadDataList() {
+
+        },
+        isExceed(row) {
+            if (row.newsTitleCh != undefined) {
+                return row.newsTitleCh.length > 40;
+            } else if (row.newsTitleEn != undefined) {
+                return row.newsTitleEn.length > 80;
+            } else {
+                return false;
+            }
+        },
+        getTitle(row) {
+
+            return row.title;
+        },
+        test(event) {
+            let yearNode = event.target.parentElement.parentElement;
+            let clazz = yearNode.className;
+            if (clazz === "year") {
+                yearNode.className = 'year close';
+            } else {
+                yearNode.className = 'year';
+            }
+        },
+        showNews(row) {
+            alert(row.title);
+        },
+
+    }
+});
